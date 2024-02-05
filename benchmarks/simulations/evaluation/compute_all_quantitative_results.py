5cd3ce2d5f9cac983644c3114017bd4dac590ddc
Onno <onno.kampman@gmail.com>
2024-02-05 14:58:02 +0800

    add sim evaluation scripts

---
+++ benchmarks/simulations/evaluation/compute_all_quantitative_results.py
@@ -0,0 +1,175 @@
+import logging
+import os
+import socket
+import sys
+
+import numpy as np
+import pandas as pd
+
+from configs.configs import get_config_dict
+from helpers.data import load_data
+from helpers.evaluation import get_corr_matrices_tril_spearman_correlation
+from helpers.evaluation import get_cov_matrices_rmse, get_corr_matrices_rmse, leave_every_other_out_split
+from helpers.evaluation import get_d2_covariance_term_correlation, get_test_log_likelihood
+from helpers.evaluation import get_d2_covariance_term_rmse, get_d2_correlation_term_rmse
+from helpers.evaluation import get_tvfc_estimates
+from helpers.synthetic_covariance_structures import get_ground_truth_covariance_structure
+
+
+def _get_performance_metric(
+        performance_metric: str,
+        predicted_covariance_structure_test_locations: np.array,
+        ground_truth_covariance_structure_test_locations: np.array,
+        y_test_locations: np.array
+) -> float:
+    """
+    Computes performance metric.
+
+    :param performance_metric:
+    :param predicted_covariance_structure_test_locations: np.array,
+    :param ground_truth_covariance_structure_test_locations:
+    :param y_test_locations:
+    :return:
+    """
+    match performance_metric:
+        case 'covariance_RMSE':
+            return get_d2_covariance_term_rmse(
+                predicted_covariance_structure_test_locations, ground_truth_covariance_structure_test_locations
+            )
+        case 'correlation_RMSE':
+            return get_d2_correlation_term_rmse(
+                predicted_covariance_structure_test_locations, ground_truth_covariance_structure_test_locations
+            )
+        case 'covariance_correlation':
+            return get_d2_covariance_term_correlation(
+                predicted_covariance_structure_test_locations, ground_truth_covariance_structure_test_locations
+            )
+        case 'covariance_matrix_RMSE':
+            return get_cov_matrices_rmse(
+                predicted_covariance_structure_test_locations, ground_truth_covariance_structure_test_locations
+            )
+        case 'correlation_matrix_RMSE':
+            return get_corr_matrices_rmse(
+                predicted_covariance_structure_test_locations, ground_truth_covariance_structure_test_locations
+            )
+        case 'correlation_tril_matrix_elements_RMSE':
+            raise NotImplementedError
+        case 'covariance_tril_matrix_elements_RMSE':
+            raise NotImplementedError
+        case 'tril_correlation':
+            return get_corr_matrices_tril_spearman_correlation(
+                predicted_covariance_structure_test_locations, ground_truth_covariance_structure_test_locations
+            )
+        case 'test_log_likelihood':
+            return get_test_log_likelihood(predicted_covariance_structure_test_locations, y_test_locations)
+        case _:
+            logging.error(f"Performance metric '{performance_metric:s}' not recognized.")
+
+
+if __name__ == "__main__":
+
+    hostname = socket.gethostname()
+
+    data_set_name = sys.argv[1]    # 'd2', 'd3d', or 'd{%d}s'
+    data_split = sys.argv[2]       # 'all', or 'LEOO'
+    experiment_data = sys.argv[3]  # e.g. 'N0200_T0200'
+
+    cfg = get_config_dict(
+        data_set_name=data_set_name,
+        experiment_data=experiment_data,
+        hostname=hostname
+    )
+    models_list = cfg['all-quantitative-results-models']
+    n_trials = int(experiment_data[-4:])
+
+    if hostname == 'hivemind':
+        if len(sys.argv) == 5:
+            noise_types = [sys.argv[4]]
+            i_trials = [int(os.environ['SLURM_ARRAY_TASK_ID']) - 1]  # to make zero-index
+        else:
+            noise_types = cfg['noise-types']
+            i_trials = range(n_trials)
+    else:
+        print('Running locally...')
+        noise_types = cfg['noise-types']
+        i_trials = range(n_trials)
+
+    for noise_type in noise_types:
+        if noise_type != 'no_noise':
+            SNR = float(noise_type[-1])
+            print('SNR', SNR)
+        else:
+            SNR = None
+        for i_trial in i_trials:
+            for perform_metric in cfg['performance-metrics']:
+                performance_df = pd.DataFrame(
+                    index=models_list,
+                    columns=cfg['all-covs-types']
+                )
+                for covs_type in cfg['all-covs-types']:
+                    data_file = os.path.join(
+                        cfg['data-dir'], noise_type, f'trial_{i_trial:03d}', f'{covs_type:s}_covariance.csv'
+                    )
+                    if not os.path.exists(data_file):
+                        logging.warning(f"Data file '{data_file:s}' not found.")
+                        performance_df.loc[:, covs_type] = np.nan
+                        continue
+                    x, y = load_data(data_file, verbose=False)  # (N, 1), (N, D)
+                    n_time_series = y.shape[1]  # D
+
+                    gt_covariance_structure = get_ground_truth_covariance_structure(
+                        covs_type=covs_type,
+                        n_samples=len(x),
+                        signal_to_noise_ratio=SNR,
+                        data_set_name=data_set_name
+                    )
+
+                    # Select train and test data through a leave-every-other-out (LEOO) split.
+                    if data_split == 'LEOO':
+                        x_train, x_test = leave_every_other_out_split(x)
+                        y_train, y_test = leave_every_other_out_split(y)
+                        gt_covariance_structure_train, gt_covariance_structure_test = leave_every_other_out_split(gt_covariance_structure)
+                    else:
+                        x_train = x_test = x
+                        y_train = y_test = y
+                        gt_covariance_structure_train = gt_covariance_structure_test = gt_covariance_structure
+
+                    for tvfc_estimation_method in models_list:
+                        estimated_cov_structure_test = get_tvfc_estimates(
+                            config_dict=cfg,
+                            model_name=tvfc_estimation_method,
+                            x_train=x_train,
+                            y_train=y_train,
+                            noise_type=noise_type,
+                            i_trial=i_trial,
+                            covs_type=covs_type,
+                            data_split=data_split,
+                            metric='covariance'
+                        )  # (N_test, D, D)
+                        if estimated_cov_structure_test is None:
+                            logging.warning(f"Estimates for '{tvfc_estimation_method:s}' not found.")
+                            performance_df.loc[tvfc_estimation_method, covs_type] = np.nan
+                            continue
+                        performance_df.loc[tvfc_estimation_method, covs_type] = _get_performance_metric(
+                            performance_metric=perform_metric,
+                            predicted_covariance_structure_test_locations=estimated_cov_structure_test,
+                            ground_truth_covariance_structure_test_locations=gt_covariance_structure_test,
+                            y_test_locations=y_test
+                        )
+                performance_df = performance_df.round(4)
+                print(performance_df.round(4))
+
+                quantitative_results_savedir = os.path.join(
+                    cfg['experiments-basedir'], noise_type, data_split,
+                    f'trial_{i_trial:03d}'
+                )
+                if not os.path.exists(quantitative_results_savedir):
+                    os.makedirs(quantitative_results_savedir)
+                quantitative_results_savepath = os.path.join(
+                    quantitative_results_savedir, f'{perform_metric:s}.csv'
+                )
+                performance_df.to_csv(
+                    quantitative_results_savepath,
+                    index=True
+                )
+                logging.info(f"Saved quantitative results in '{quantitative_results_savedir:s}'.")
---
+++ benchmarks/simulations/evaluation/compute_average_quantitative_results.py
@@ -0,0 +1,188 @@
+import logging
+import os
+import socket
+import sys
+
+from fcest.helpers.array_operations import get_all_lower_triangular_indices_tuples
+import numpy as np
+import pandas as pd
+import pingouin
+
+from configs.configs import get_config_dict
+
+
+# TODO: instead of saving a mean and std/ste csv file, it would be good to save all individual trials to allow for violin plot
+
+
+if __name__ == "__main__":
+
+    hostname = socket.gethostname()
+
+    data_set_name = sys.argv[1]    # 'd2', 'd3d', or 'd3s'
+    data_split = sys.argv[2]       # 'all', or 'LEOO'
+    experiment_data = sys.argv[3]  # e.g. 'N0200_T0100'
+
+    cfg = get_config_dict(
+        data_set_name=data_set_name,
+        experiment_data=experiment_data,
+        hostname=hostname
+    )
+    models_list = cfg['all-quantitative-results-models']
+    n_trials = int(experiment_data[-4:])
+
+    for noise_type in cfg['noise-types']:
+
+        git_results_savedir = os.path.join(
+            cfg['git-results-basedir'], noise_type, data_split
+        )
+        if not os.path.exists(git_results_savedir):
+            os.makedirs(git_results_savedir)
+
+        for performance_metric in cfg['performance-metrics']:
+
+            print(f"\n> Noise type:         {noise_type:s}")
+            print(f"> Performance metric: {performance_metric:s}")
+
+            all_trials_results = []
+            for i_trial in range(n_trials):
+                quantitative_results_savepath = os.path.join(
+                    cfg['experiments-basedir'], noise_type, data_split,
+                    f'trial_{i_trial:03d}', f'{performance_metric:s}.csv'
+                )
+                
+                single_trial_results_df = pd.read_csv(
+                    quantitative_results_savepath,
+                    index_col=0
+                )  # (n_models, n_covs_types) - can contain NaNs!
+                assert single_trial_results_df.shape == (len(models_list), len(cfg['all-covs-types']))
+
+                # missing_cov_types = pd.isnull(single_trial_results_df).all().nonzero()[0]
+                # missing_models = pd.isnull(single_trial_results_df).all().nonzero()[0]
+                
+                all_trials_results.append(single_trial_results_df.values)
+
+            all_trials_results = np.array(all_trials_results)  # (n_trials, n_models, n_covs_types)
+            print('all_results:', all_trials_results.shape)
+
+            # Run statistical significance tests between all estimation methods.
+            for _, cov_type in enumerate(cfg['plot-covs-types']):
+
+                i_all_cov_type = cfg['all-covs-types'].index(cov_type)
+                print(i_all_cov_type, cov_type, cfg['all-covs-types'])
+
+                all_results_cov_type_df = pd.DataFrame(
+                    all_trials_results[:, :, i_all_cov_type],
+                    columns=models_list
+                )  # (n_trials, n_models)
+
+                all_method_t_values_df = pd.DataFrame(
+                    np.nan,
+                    index=models_list,
+                    columns=models_list
+                )
+                all_method_significances_df = pd.DataFrame(
+                    np.nan,
+                    index=models_list,
+                    columns=models_list
+                )
+                all_method_effect_sizes_df = pd.DataFrame(
+                    np.nan,
+                    index=models_list,
+                    columns=models_list
+                )
+                for (i_model, j_model) in get_all_lower_triangular_indices_tuples(len(models_list)):
+                    results_i = all_trials_results[:, i_model, i_all_cov_type]
+                    results_j = all_trials_results[:, j_model, i_all_cov_type]
+                    if np.isnan(results_i).all() or np.isnan(results_j).all():
+                        continue
+                    ttest_results_pingouin_df = pingouin.ttest(
+                        results_i, results_j,
+                        alternative='two-sided',
+                        correction='auto'
+                    )
+                    all_method_t_values_df.iloc[i_model, j_model] = all_method_t_values_df.iloc[j_model, i_model] = ttest_results_pingouin_df['T'].values
+                    all_method_significances_df.iloc[i_model, j_model] = all_method_significances_df.iloc[j_model, i_model] = ttest_results_pingouin_df['p-val'].values
+                    all_method_effect_sizes_df.iloc[i_model, j_model] = all_method_effect_sizes_df.iloc[j_model, i_model] = ttest_results_pingouin_df['cohen-d'].values
+                print(all_method_t_values_df.round(2))
+                print(all_method_significances_df.round(4))
+                print(all_method_effect_sizes_df.round(2))
+
+                tvals_filename = f'{cov_type:s}_between_method_performance_T.csv'
+                pvals_filename = f'{cov_type:s}_between_method_performance_pvals.csv'
+                effect_sizes_filename = f'{cov_type:s}_between_method_performance_cohen_d.csv'
+                ttest_results_savedir = os.path.join(
+                    git_results_savedir, 'ttests', performance_metric
+                )
+                if not os.path.exists(ttest_results_savedir):
+                    os.makedirs(ttest_results_savedir)
+                all_method_t_values_df.to_csv(
+                    os.path.join(ttest_results_savedir, tvals_filename),
+                    index=True,
+                    float_format='%.2f'
+                )
+                all_method_significances_df.to_csv(
+                    os.path.join(ttest_results_savedir, pvals_filename),
+                    index=True,
+                    float_format='%.4f'
+                )
+                all_method_effect_sizes_df.to_csv(
+                    os.path.join(ttest_results_savedir, effect_sizes_filename),
+                    index=True,
+                    float_format='%.2f'
+                )
+                logging.info(f"Saved {data_split:s} t-test results '{tvals_filename:s}', '{pvals_filename:s}', and '{effect_sizes_filename:s}' in '{ttest_results_savedir:s}'.")
+
+                pairwise_ttest_results_pingouin_df = pingouin.ptests(
+                    all_results_cov_type_df,
+                    decimals=4,
+                    padjust='bonf',
+                    stars=False,
+                )
+                print(pairwise_ttest_results_pingouin_df.round(4))
+
+                ptests_results_filename = f'{cov_type:s}_between_method_performance_ptests.csv'
+                pairwise_ttest_results_pingouin_df.to_csv(
+                    os.path.join(ttest_results_savedir, ptests_results_filename),
+                    index=True,
+                    float_format='%.4f'
+                )
+                logging.info(f"Saved {data_split:s} t-test ptests results '{ptests_results_filename:s}' in '{ttest_results_savedir:s}'.")
+
+            # re-use the last df for its row and column names
+            mean_df = single_trial_results_df.copy()
+            mean_df.iloc[:, :] = np.nanmean(
+                all_trials_results,
+                axis=0
+            )
+            mean_df['average'] = mean_df.mean(axis=1)
+            mean_df.to_csv(
+                os.path.join(git_results_savedir, f'{performance_metric:s}_mean.csv'),
+                index=True,
+                float_format='%.3f'
+            )
+
+            # re-use the last df for its row and column names
+            std_df = single_trial_results_df.copy()
+            std_df.iloc[:, :] = np.nanstd(
+                all_trials_results,
+                axis=0
+            )
+            std_df['average'] = std_df.mean(axis=1)
+            std_df.to_csv(
+                os.path.join(git_results_savedir, f'{performance_metric:s}_std.csv'),
+                index=True,
+                float_format='%.3f'
+            )
+
+            # Standard error.
+            # re-use the last df for its row and column names
+            se_df = single_trial_results_df.copy()
+            se_df.iloc[:, :] = np.nanstd(all_trials_results, axis=0) / np.sqrt(all_trials_results.shape[0])
+            se_df['average'] = se_df.mean(axis=1)
+            se_df.to_csv(
+                os.path.join(git_results_savedir, f'{performance_metric:s}_se.csv'),
+                index=True,
+                float_format='%.3f'
+            )
+
+            logging.info(f"Saved average '{performance_metric:s}' results in '{git_results_savedir:s}'.")

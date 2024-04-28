19dfad8d531eb1df03e8dab76bc3dd9377393606
Onno Kampman <onno.kampman@gmail.com>
2024-04-28 21:35:31 +0800

    minor fix

--- benchmarks/fmri/rs/HCP_PTN1200_recon2/kernel_analysis/save_SVWP_kernel_params.py
+++ benchmarks/fmri/rs/HCP_PTN1200_recon2/kernel_analysis/save_SVWP_kernel_params.py
@@ -29,18 +29,23 @@ if __name__ == "__main__":
     if not os.path.exists(kernel_params_savedir):
         os.makedirs(kernel_params_savedir)
 
-    n_subjects = cfg['n-subjects']
+    num_subjects = cfg['n-subjects']
     all_subjects_list = get_human_connectome_project_subjects(
-        data_dir=cfg['data-dir'], first_n_subjects=n_subjects, as_ints=True
+        data_dir=cfg['data-dir'],
+        first_n_subjects=num_subjects,
+        as_ints=True,
     )
 
     # TODO: refactor so we only load each model once?
     for kernel_param in cfg['kernel-params']:
-        kernel_params_df = pd.DataFrame(index=all_subjects_list, columns=cfg['scan-ids'])
+        kernel_params_df = pd.DataFrame(
+            index=all_subjects_list, 
+            columns=cfg['scan-ids'],
+        )
         print('\nKERNEL PARAM:', kernel_param, '\n')
 
         for i_subject, subject in enumerate(all_subjects_list):
-            print(f'\n> SUBJECT {i_subject+1:d} / {n_subjects:d}: {subject:d}')
+            print(f'\n> SUBJECT {i_subject+1:d} / {num_subjects:d}: {subject:d}')
 
             for scan_id in cfg['scan-ids']:
                 model_savedir = os.path.join(
@@ -52,8 +57,8 @@ if __name__ == "__main__":
                 kernel_params_df.loc[subject, scan_id] = m_dict[kernel_param]
 
         kernel_params_df_filename = f'{kernel_param:s}_kernel_params.csv'
-        kernel_params_df.to_csv(
+        kernel_params_df.astype(float).to_csv(
             os.path.join(kernel_params_savedir, kernel_params_df_filename),
-            float_format='%.3f'
+            float_format='%.3f',
         )
         logging.info(f"Saved kernel params table '{kernel_params_df_filename:s}' to '{kernel_params_savedir:s}'.")
